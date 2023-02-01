5f7eacffc6ab298cb77b9c8f9c886ed853246db8
murat-dede <murat1_dede@hotmail.com>
2023-02-01 12:58:33 +0300

    s9 cha
    Grid ayarı olmadı

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // önerilen başlangıç stateleri
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialGridX= 1;
+const initialGridY= 1;
+const gridSizeX= 3;
+const gridSizeY= 3;
+const globalGrid=[
+  [1,1],
+  [2,1],
+  [3,1],
+  [1,2],
+  [2,2],
+  [3,2],
+  [1,3],
+  [2,3],
+  [3,3]
+];
 
 export default function AppFunctional(props) {
+  const [gridState,setGridState] =useState(globalGrid[initialIndex]);
+
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
-
+const[click,setclick]=useState(4);
   function getXY() {
+    
+    return 
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
   }
@@ -34,6 +53,25 @@ export default function AppFunctional(props) {
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
+    const nereye = evt.target.id
+    console.log('ilerle fonksiyonu',nereye);
+    const gridStateX=gridState[1];
+    const gridStateY=gridState[0];
+    if(gridStateX<gridSizeX&&nereye=='left'){
+      setGridState([gridStateX-1,gridStateY])
+    }
+    if(gridStateX<gridSizeX&&nereye=='right'){
+      setGridState([gridStateX+1,gridStateY])
+    }
+    if(gridStateY<gridSizeY&&nereye=='up'){
+      setGridState([gridStateX,gridStateY+1])
+    }
+    if(gridStateY<gridSizeY&&nereye=='down'){
+      setGridState([gridStateX,gridStateY-1])
+    }
+    console.log('ilerle fonksiyonu',gridStateX,gridStateY);
+
+
   }
 
   function onChange(evt) {
@@ -42,6 +80,7 @@ export default function AppFunctional(props) {
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    evt.preventDefault();
   }
 
   return (
@@ -63,15 +102,23 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={ilerle} id="left">
+          SOL
+          </button>
+        <button onClick={ilerle} id="up">
+          YUKARI
+          </button>
+        <button onClick={ilerle} id="right">
+          SAĞ
+          </button>
+        <button onClick={ilerle} id="down">
+          AŞAĞI
+          </button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
-      <form>
+      <form onSubmit={onSubmit} >
         <input id="email" type="email" placeholder="email girin"></input>
-        <input id="submit" type="submit"></input>
+        <input id="submit" type="submit" ></input>
       </form>
     </div>
   )
